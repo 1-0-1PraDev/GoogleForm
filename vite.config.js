7b91d32e8573cc888275859236d1d0bc9406d3a6
1-0-1PraDev <pranayhusukale99@gmail.com>
2025-03-13 09:35:54 +0530

    Fixes some directory issues during deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
+  base: "/GoogleForm/",
   publicDir: "../public",
   build: {
     outDir: "../dist",
+    assetsDir: "assets",
   },
   server: {
     port: 9000,
